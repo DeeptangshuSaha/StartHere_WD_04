d887c07036c43a8ef949f6d482be15ba4e1ede86
LichtBach <deeptangshusaha62@gmail.com>
2024-08-25 19:07:07 +0530

    Cleaning up timer.jsx

--- src/timer.tsx
+++ src/timer.tsx
@@ -4,9 +4,9 @@ const Timer=(a: string | Date) => {
     const Ref = useRef(null);
     const [timer, setTimer] = useState([]);
 
-    const getTimeRemaining = (e) => {
+    const getTimeRemaining = (e: string | Date) => {
         const total =
-            Date.parse(e) - Date.parse(new Date());
+            Date.parse(e as string) - Date.parse(Date());
         const seconds = Math.floor((total / 1000) % 60);
         const minutes = Math.floor((total / 1000 / 60) % 60);
         const hours = Math.floor((total / 1000 / 60 / 60) % 24);
@@ -20,32 +20,34 @@ const Timer=(a: string | Date) => {
         };
     };
 
-    const startTimer = (e) => {
+    const startTimer = (e: string | Date) => {
         const { total, days, hours, minutes, seconds } =
             getTimeRemaining(e);
         if (total >= 0) {
-            setTimer([days, hours, minutes, seconds]);
-        } else {
-            setTimer("Done");
+            const les = [days, hours, minutes, seconds];
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-expect-error
+            setTimer(les);
         }
     };
 
-    const clearTimer = (e) => {
+    const clearTimer = (e: string | Date) => {
         setTimer([]);
         if (Ref.current) clearInterval(Ref.current);
-        const id = setInterval(() => {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-expect-error
+        Ref.current = setInterval(() => {
             startTimer(e);
         }, 1000);
-        Ref.current = id;
     };
 
     const getDeadTime = () => {
         //return new Date(Date.parse(new Date()) + 2 * 24 * 60 * 60 * 1000); //return new Date(Date.parse(new Date()) + (days) * (hours) * (minutes) * (seconds) * 1000);
-        return new Date(Date.parse(a));
+        return new Date(Date.parse(a as string));
     };
     useEffect(() => {
         clearTimer(getDeadTime());
-    }, []);
+    });
     return timer;
 }
 export default Timer;
